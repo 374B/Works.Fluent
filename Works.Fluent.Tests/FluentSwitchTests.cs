f5a3bfe420369bdd63abf80b417aa70677d2d64e
Wayne Davidson <wdavos@gmail.com>
2014-01-18 17:39:26 +1100

    FluentSwitch impl & tests

--- Works.Fluent.Tests/FluentSwitchTests.cs
+++ Works.Fluent.Tests/FluentSwitchTests.cs
@@ -7,21 +7,6 @@ namespace Works.Fluent.Tests
   internal class FluentSwitchTests
   {
     [Test]
-    public void Case_Should_Match_Null_Value()
-    {
-      bool handled = false;
-
-      FluentSwitch<string>
-        .Comparer((a, b) => { return a == b; })
-        .Case((string)null)
-          .Do(() => { handled = true; })
-        .Default(() => { })
-        .Switch(null);
-
-      Assert.IsTrue(handled);
-    }
-
-    [Test]
     public void Case_Should_Throw_ArgumentNullException_When_Values_Is_Null()
     {
       Assert.Throws<ArgumentNullException>(() =>
@@ -30,39 +15,6 @@ namespace Works.Fluent.Tests
           .Comparer((a, b) => { return a == b; })
           .Case((IEnumerable<string>)null);
       });
-
-      //FluentSwitch<string>
-      //  .Comparer((a, b) => { return a == b; })
-      //  .Case(nullCase)
-      //    .Do(() =>
-      //    {
-      //      handled = true;
-      //    })
-      //  .Default(() =>
-      //  {
-      //  })
-      //  .Switch(nullTest);
-
-      //Assert.IsTrue(handled);
-    }
-
-    [Test]
-    public void Case_With_Single_Arg_Should_Execute_On_Match()
-    {
-      bool executed = false;
-
-      FluentSwitch<string>
-        .Comparer((a, b) => { return a == b; })
-        .Case("A")
-          .Do(() => { executed = true; })
-        .Default(() => { });
-
-      Assert.IsTrue(executed);
-    }
-
-    [Test]
-    public void Case_With_Single_Arg_Should_Not_Execute_When_Not_Matched()
-    {
     }
 
     [Test]
@@ -85,7 +37,7 @@ namespace Works.Fluent.Tests
     }
 
     [Test]
-    public void Default_Should_Allow_Null()
+    public void Default_Should_Allow_Null_Action()
     {
       Assert.DoesNotThrow(() =>
       {
@@ -94,6 +46,335 @@ namespace Works.Fluent.Tests
           .Case("A")
             .Do(() => { })
           .Default(null);
+      });
+    }
+
+    [Test]
+    public void Do_Should_Allow_Null_Action()
+    {
+      Assert.DoesNotThrow(() =>
+      {
+        FluentSwitch<string>
+          .Comparer((a, b) => { return a == b; })
+          .Case("A")
+            .Do((Action)null);
+      });
+    }
+
+    [Test]
+    public void Do_T_Should_Allow_Null_Action()
+    {
+      Assert.DoesNotThrow(() =>
+      {
+        FluentSwitch<string>
+          .Comparer((a, b) => { return a == b; })
+          .Case("A")
+            .Do((Action<string>)null);
+      });
+    }
+
+    [Test]
+    public void Null_Default_Action_Should_Be_Handled()
+    {
+      Assert.DoesNotThrow(() =>
+      {
+        FluentSwitch<string>
+          .Comparer((a, b) => { return a == b; })
+          .Case("A")
+            .Do(() => { })
+            .Default(null)
+          .Switch("X");
+      });
+    }
+
+    [Test]
+    public void Null_Do_Action_Should_Be_Handled()
+    {
+      Assert.DoesNotThrow(() =>
+      {
+        FluentSwitch<string>
+          .Comparer((a, b) => { return a == b; })
+          .Case("A")
+            .Do((Action)null)
+            .Default(() => { })
+          .Switch("A");
+      });
+    }
+
+    [Test]
+    public void Null_Do_T_Action_Should_Be_Handled()
+    {
+      Assert.DoesNotThrow(() =>
+      {
+        FluentSwitch<string>
+          .Comparer((a, b) => { return a == b; })
+          .Case("A")
+            .Do((Action<string>)null)
+            .Default(() => { })
+          .Switch("A");
+      });
+    }
+
+    [Test]
+    public void Switch_Should_Execute_Matched_Case()
+    {
+      bool executed = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { executed = true; })
+        .Default(() => { })
+        .Switch("A");
+
+      Assert.IsTrue(executed);
+    }
+
+    [Test]
+    public void Switch_Should_Match_Null_Value()
+    {
+      bool handled = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case((string)null)
+          .Do(() => { handled = true; })
+        .Default(() => { })
+        .Switch(null);
+
+      Assert.IsTrue(handled);
+    }
+
+    [Test]
+    public void Switch_Should_Not_Execute_Unmatched_Case()
+    {
+      bool executed = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { executed = true; })
+        .Default(() => { })
+        .Switch("B");
+
+      Assert.IsFalse(executed);
+    }
+
+    [Test]
+    public void SwitchAll_Should_Execute_All_Matched()
+    {
+      bool aExecuted = false;
+      bool bExecuted = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { aExecuted = true; })
+        .Case("B")
+          .Do(() => { bExecuted = true; })
+        .Default(() => { })
+        .SwitchAll("A", "B");
+
+      Assert.IsTrue(aExecuted && bExecuted);
+    }
+
+    [Test]
+    public void SwitchAll_Should_Execute_Default_When_No_Matches()
+    {
+      bool executed = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { })
+        .Case("B")
+          .Do(() => { })
+        .Default(() => { executed = true; })
+        .SwitchAll("1", "2");
+
+      Assert.IsTrue(executed);
+    }
+
+    [Test]
+    public void SwitchAll_Should_Not_Execute_Default_When_A_Match_Is_Made()
+    {
+      bool executed = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { })
+        .Case("B")
+          .Do(() => { })
+        .Default(() => { executed = true; })
+        .SwitchAll("A", "B");
+
+      Assert.IsFalse(executed);
+    }
+
+    [Test]
+    public void SwitchAll_Should_Not_Execute_Unmatched()
+    {
+      bool aExecuted = false;
+      bool bExecuted = false;
+      bool cExecuted = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { aExecuted = true; })
+        .Case("B")
+          .Do(() => { bExecuted = true; })
+        .Case("C")
+          .Do(() => { cExecuted = true; })
+        .Default(() => { })
+        .SwitchAll("A", "B");
+
+      Assert.IsTrue(aExecuted && bExecuted && !cExecuted);
+    }
+
+    [Test]
+    public void SwitchAll_Should_Throw_ArgumentNullException_When_Params_Null()
+    {
+      Assert.Throws<ArgumentNullException>(() =>
+      {
+        FluentSwitch<string>
+          .Comparer((a, b) => { return a == b; })
+          .Case("A")
+            .Do(() => { })
+          .Default(() => { })
+          .SwitchAll(null);
+      });
+    }
+
+    [Test]
+    public void SwitchAll_Should_Throw_ArgumentNullException_When_Values_Null()
+    {
+      List<string> values = null;
+
+      Assert.Throws<ArgumentNullException>(() =>
+      {
+        FluentSwitch<string>
+          .Comparer((a, b) => { return a == b; })
+          .Case("A")
+            .Do(() => { })
+          .Default(() => { })
+          .SwitchAll(values);
+      });
+    }
+
+    [Test]
+    public void SwitchFirst_Should_Execute_Matched_Case()
+    {
+      bool executed = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { executed = true; })
+        .Default(() => { })
+        .SwitchFirst("B", "A");
+
+      Assert.IsTrue(executed);
+    }
+
+    [Test]
+    public void SwitchFirst_Should_Executed_Default_When_No_Matches()
+    {
+      bool executed = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { })
+        .Case("B")
+          .Do(() => { })
+        .Default(() => { executed = true; })
+        .SwitchFirst("1", "2");
+
+      Assert.IsTrue(executed);
+    }
+
+    [Test]
+    public void SwitchFirst_Should_Not_Execute_Default_When_A_Match_Is_Made()
+    {
+      bool executed = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { })
+        .Case("B")
+          .Do(() => { })
+        .Default(() => { executed = true; })
+        .SwitchFirst("A", "B");
+
+      Assert.IsFalse(executed);
+    }
+
+    [Test]
+    public void SwitchFirst_Should_Not_Execute_Subsequent_Matches()
+    {
+      bool aExecuted = false;
+      bool bExecuted = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { aExecuted = true; })
+        .Case("B")
+          .Do(() => { bExecuted = true; })
+        .Default(() => { })
+        .SwitchFirst("A", "B");
+
+      Assert.IsTrue(aExecuted && !bExecuted);
+    }
+
+    [Test]
+    public void SwitchFirst_Should_Not_Execute_Unmatched()
+    {
+      bool aExecuted = false;
+      bool bExecuted = false;
+
+      FluentSwitch<string>
+        .Comparer((a, b) => { return a == b; })
+        .Case("A")
+          .Do(() => { aExecuted = true; })
+        .Case("B")
+          .Do(() => { bExecuted = true; })
+        .Default(() => { })
+        .SwitchFirst("1", "2");
+
+      Assert.IsFalse(aExecuted || bExecuted);
+    }
+
+    [Test]
+    public void SwitchFirst_Should_Throw_ArgumentNullException_When_Params_Null()
+    {
+      Assert.Throws<ArgumentNullException>(() =>
+      {
+        FluentSwitch<string>
+          .Comparer((a, b) => { return a == b; })
+          .Case("A")
+            .Do(() => { })
+          .Default(() => { })
+          .SwitchFirst(null);
+      });
+    }
+
+    [Test]
+    public void SwitchFirst_Should_Throw_ArgumentNullException_When_Values_Null()
+    {
+      List<string> values = null;
+
+      Assert.Throws<ArgumentNullException>(() =>
+      {
+        FluentSwitch<string>
+          .Comparer((a, b) => { return a == b; })
+          .Case("A")
+            .Do(() => { })
+          .Default(() => { })
+          .SwitchFirst(values);
       });
     }
   }
--- Works.Fluent/FluentSwitch.cs
+++ Works.Fluent/FluentSwitch.cs
@@ -24,14 +24,39 @@ namespace Works.Fluent
     [EditorBrowsable(EditorBrowsableState.Never)]
     public interface ISwitch<TI> : IFluentInterface
     {
+      /// <summary>
+      /// Swith the Value
+      /// </summary>
+      /// <param name="Value"></param>
+      /// <returns></returns>
       ISwitch<TI> Switch(TI Value);
 
+      /// <summary>
+      /// Switch all values in the Values collection
+      /// </summary>
+      /// <param name="Values"></param>
+      /// <returns></returns>
       ISwitch<TI> SwitchAll(params TI[] Values);
 
+      /// <summary>
+      /// Switch all values in the Values collection
+      /// </summary>
+      /// <param name="Values"></param>
+      /// <returns></returns>
       ISwitch<TI> SwitchAll(IEnumerable<TI> Values);
 
+      /// <summary>
+      /// Switch the first matched value in the Values collection
+      /// </summary>
+      /// <param name="Values"></param>
+      /// <returns></returns>
       ISwitch<TI> SwitchFirst(params TI[] Values);
 
+      /// <summary>
+      /// Switch the first matched value in the Values collection
+      /// </summary>
+      /// <param name="Values"></param>
+      /// <returns></returns>
       ISwitch<TI> SwitchFirst(IEnumerable<TI> Values);
     }
 
@@ -73,8 +98,10 @@ namespace Works.Fluent
 
     private class FluentSwitchImpl : ISwitchCase<T>, ISwitchCaseOrDefault<T>, ISwitch<T>, ISwitchDo<T>
     {
+      private readonly List<CaseDefinition> cases = new List<CaseDefinition>();
+      private readonly List<CaseDefinitionWithReturn> casesWithDefs = new List<CaseDefinitionWithReturn>();
       private readonly Func<T, T, bool> comparer;
-
+      private readonly List<T> current = new List<T>();
       private Action defaultAction;
 
       public FluentSwitchImpl(Func<T, T, bool> Comparer)
@@ -84,6 +111,8 @@ namespace Works.Fluent
 
       public ISwitchDo<T> Case(T Value)
       {
+        current.Clear();
+        current.Add(Value);
         return this;
       }
 
@@ -92,6 +121,8 @@ namespace Works.Fluent
         if (Values == null)
           throw new ArgumentNullException("Values", "Values must not be null");
 
+        current.Clear();
+        current.AddRange(Values);
         return this;
       }
 
@@ -100,6 +131,8 @@ namespace Works.Fluent
         if (Values == null)
           throw new ArgumentNullException("Values", "Values must not be null");
 
+        current.Clear();
+        current.AddRange(Values);
         return this;
       }
 
@@ -111,37 +144,195 @@ namespace Works.Fluent
 
       public ISwitchCaseOrDefault<T> Do(Action Action)
       {
+        foreach (var value in current)
+        {
+          var local = value;
+          cases.Add(new CaseDefinition(local, Action));
+        }
+
+        current.Clear();
         return this;
       }
 
       public ISwitchCaseOrDefault<T> Do(Action<T> Action)
       {
+        foreach (var value in current)
+        {
+          var local = value;
+          casesWithDefs.Add(new CaseDefinitionWithReturn(local, Action));
+        }
+
+        current.Clear();
         return this;
       }
 
       public ISwitch<T> Switch(T Value)
       {
+        CaseDefinitionWithReturn matchWithReturn;
+
+        if (TryMatchCase(Value, out matchWithReturn))
+        {
+          ExecuteAction(Value, matchWithReturn.Action);
+        }
+        else
+        {
+          CaseDefinition match;
+          if (TryMatchCase(Value, out match))
+          {
+            ExecuteAction(match.Action);
+          }
+          else
+          {
+            ExecuteAction(defaultAction);
+          }
+        }
+
         return this;
       }
 
       public ISwitch<T> SwitchAll(params T[] Values)
       {
-        return this;
+        if (Values == null)
+          throw new ArgumentNullException("Values", "Values must not be null");
+
+        return SwitchEnumerable(Values, false);
       }
 
       public ISwitch<T> SwitchAll(IEnumerable<T> Values)
       {
-        return this;
+        if (Values == null)
+          throw new ArgumentNullException("Values", "Values must not be null");
+
+        return SwitchEnumerable(Values, false);
       }
 
       public ISwitch<T> SwitchFirst(params T[] Values)
       {
-        return this;
+        if (Values == null)
+          throw new ArgumentNullException("Values", "Values must not be null");
+
+        return SwitchEnumerable(Values, true);
       }
 
       public ISwitch<T> SwitchFirst(IEnumerable<T> Values)
       {
+        if (Values == null)
+          throw new ArgumentNullException("Values", "Values must not be null");
+
+        return SwitchEnumerable(Values, true);
+      }
+
+      private void ExecuteAction(Action Action)
+      {
+        if (Action != null)
+        {
+          Action();
+        }
+      }
+
+      private void ExecuteAction(T Value, Action<T> Action)
+      {
+        if (Action != null)
+        {
+          Action(Value);
+        }
+      }
+
+      private ISwitch<T> SwitchEnumerable(IEnumerable<T> Values, bool BreakOnMatch)
+      {
+        bool anyMatches = false;
+
+        foreach (var v in Values)
+        {
+          var local = v;
+          CaseDefinitionWithReturn matchWithReturn;
+          if (TryMatchCase(local, out matchWithReturn))
+          {
+            anyMatches = true;
+            ExecuteAction(local, matchWithReturn.Action);
+            if (BreakOnMatch)
+            {
+              break;
+            }
+          }
+          else
+          {
+            CaseDefinition match;
+            if (TryMatchCase(local, out match))
+            {
+              anyMatches = true;
+              ExecuteAction(match.Action);
+              if (BreakOnMatch)
+              {
+                break;
+              }
+            }
+          }
+        }
+
+        if (!anyMatches)
+        {
+          ExecuteAction(defaultAction);
+        }
+
         return this;
+      }
+
+      private bool TryMatchCase(T Value, out CaseDefinition Match)
+      {
+        Match = null;
+        foreach (var c in cases)
+        {
+          if (comparer(c.Value, Value))
+          {
+            Match = c;
+            break;
+          }
+        }
+
+        return Match != null;
+      }
+
+      private bool TryMatchCase(T Value, out CaseDefinitionWithReturn Match)
+      {
+        Match = null;
+
+        foreach (var c in casesWithDefs)
+        {
+          if (comparer(c.Value, Value))
+          {
+            Match = c;
+            break;
+          }
+        }
+
+        return Match != null;
+      }
+
+      private class CaseDefinition
+      {
+        public CaseDefinition(T Value, Action Action)
+        {
+          this.Value = Value;
+          this.Action = Action;
+        }
+
+        public Action Action { get; private set; }
+
+        public T Value { get; private set; }
+      }
+
+      private class CaseDefinitionWithReturn
+      {
+        public CaseDefinitionWithReturn(T Value, Action<T> Action)
+        {
+          this.Value = Value;
+          this.Action = Action;
+        }
+
+        public Action<T> Action { get; private set; }
+
+        public T Value { get; private set; }
       }
     }
 
