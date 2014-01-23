0ffb2f832d99e37109d5ea5de70d5e8a13207190
wdavo <wdavos@gmail.com>
2014-01-23 16:10:32 +1100

    Changed to auto increment versioning Fixed bug in samples

--- Works.Fluent.Samples/FluentSwitch/Strings.cs
+++ Works.Fluent.Samples/FluentSwitch/Strings.cs
@@ -1,44 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Works.Fluent.Samples.FluentSwitch
 {
   public static class Strings
   {
-    public static void Simple()
-    {
-      var value = "C";
-
-      switch (value)
-      {
-        case "A":
-          HandleA();
-          break;
-
-        case "B":
-          HandleB();
-          break;
-
-        case "C":
-          HandleC();
-          break;
-
-        default:
-          HandleOther();
-          break;
-      }
-
-      FluentSwitch<string>
-        .Comparer(String.Equals)
-        .Case("A").Do(HandleA)
-        .Case("B").Do(HandleB)
-        .Case("C").Do(HandleC)
-        .Default(HandleOther)
-        .Switch(value);
-    }
-
-    public void Case_Insensitive()
+    public static void Case_Insensitive()
     {
       var value = "C";
 
@@ -69,7 +36,7 @@ namespace Works.Fluent.Samples.FluentSwitch
         .Switch(value);
     }
 
-    public void List_All_Matches()
+    public static void List_All_Matches()
     {
       var values = new List<string> { "A", "B", "C" };
 
@@ -104,7 +71,7 @@ namespace Works.Fluent.Samples.FluentSwitch
         .SwitchAll(values);
     }
 
-    public void List_First_Match()
+    public static void List_First_Match()
     {
       var values = new List<string> { "A", "B", "C" };
 
@@ -142,6 +109,38 @@ namespace Works.Fluent.Samples.FluentSwitch
         .Case("C").Do(HandleC)
         .Default(HandleOther)
         .SwitchFirst(values);
+    }
+
+    public static void Simple()
+    {
+      var value = "C";
+
+      switch (value)
+      {
+        case "A":
+          HandleA();
+          break;
+
+        case "B":
+          HandleB();
+          break;
+
+        case "C":
+          HandleC();
+          break;
+
+        default:
+          HandleOther();
+          break;
+      }
+
+      FluentSwitch<string>
+        .Comparer(String.Equals)
+        .Case("A").Do(HandleA)
+        .Case("B").Do(HandleB)
+        .Case("C").Do(HandleC)
+        .Default(HandleOther)
+        .Switch(value);
     }
 
     private static void HandleA()
